dbf3351a113e3724a7407bca329cda9ab31c3cea
masoud <bimar.masoud@golrang.com>
2019-07-30 18:22:56 +0430

    min-max sample & disable days

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,6 +10,9 @@ export class TestComponent implements OnInit {
 
   date;
   date2;
+  minDate = moment().add(-2, 'days');
+  maxDate= moment().add(3, 'days');
+  invalidDates = [moment()];
   constructor() {
     this.date = moment().locale('fa');
     this.date2 = moment().locale('en');
